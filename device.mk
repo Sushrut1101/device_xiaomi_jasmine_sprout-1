e8d49455c77806b99f2fea5743bd65a0b55c6ddc
Sushrut1101 <guptasushrut@gmail.com>
2022-02-02 22:56:31 +0530

    jasmine_sprout: Use qcom-caf Bootctrl
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    Signed-off-by: RealAkira <beastdark704@gmail.com>

--- device.mk
+++ device.mk
@@ -26,3 +26,8 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libhardware_legacy.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl    
